73d2d4135ba192ae197bf9973685d93b12c6bae5
Alexei Garban <garban.valdeon@gmail.com>
2024-07-10 20:31:18 +0200

    privacy

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ export default function Header() {
 
 <nav class="bg-white border-gray-200 dark:bg-gray-900">
   <div className="max-w-screen-md flex flex-wrap items-center justify-between mx-auto py-4">
-    <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+    <a href="https://electricidad.vercel.app/" className="flex items-center space-x-3 rtl:space-x-reverse">
         {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" /> */}
         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white md:px-0 px-5">LuzAhora</span>
     </a>
