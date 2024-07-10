c2599338bab3613001ae5deff9556af6d9d60063
Alexei Garban <garban.valdeon@gmail.com>
2024-07-10 20:15:26 +0200

    privacy

---
+++ src/app/privacy-policy/page.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import Hero from '@/components/Hero'
+export default function page() {
+  return (
+    <div className="max-w-screen-md mx-auto ">
+    {/*     <Hero /> */}
+
+        <section className="bg-blue-50 p-5 rounded-lg my-10">
+        <div className="max-w-screen-md mx-auto ">
+            <h3 className="font-bold text-2xl mb-10">Privacy Policy / Políticas de privacidad</h3>
+            <p>
+            1. Introducción
+
+ En LuzAhora respetamos tu privacidad y nos comprometemos a proteger la información personal que compartes con nosotros. Esta política de privacidad explica cómo recopilamos, usamos y protegemos tu información cuando visitas nuestro sitio web, que muestra el precio de la luz en España utilizando datos públicos y emplea Google Analytics y Google AdSense.
+
+2. Información que Recopilamos
+
+Nuestro sitio web recopila dos tipos de información:
+
+Información No Personal: Incluye datos que no permiten identificarte personalmente, como información demográfica, datos de navegación (tipo de navegador, idioma del navegador, páginas vistas, tiempo de visita, etc.), y otra información de carácter técnico.
+Cookies y Tecnologías Similares: Utilizamos cookies y tecnologías similares para recopilar información sobre tu actividad en nuestro sitio web. Puedes controlar el uso de cookies a través de la configuración de tu navegador.
+3. Uso de la Información Recopilada
+
+La información recopilada se utiliza para los siguientes propósitos:
+
+Datos Públicos sobre el Precio de la Luz: Utilizamos datos públicos para mostrar el precio de la luz en España. Estos datos no están asociados con información personal de los usuarios.
+Google Analytics: Usamos Google Analytics para analizar cómo los usuarios interactúan con nuestro sitio web. Google Analytics recopila información como la frecuencia con la que los usuarios visitan el sitio, las páginas visitadas y otros datos de uso. Esta información nos ayuda a mejorar el funcionamiento y el contenido de nuestro sitio web.
+Google AdSense: Usamos Google AdSense para mostrar anuncios en nuestro sitio web. Google AdSense puede utilizar cookies y balizas web para mostrar anuncios personalizados basados en tus intereses y tu comportamiento de navegación.
+4. Compartir Información
+
+No vendemos, comercializamos ni transferimos tu información personal a terceros, excepto en los siguientes casos:
+
+Proveedores de Servicios: Podemos compartir tu información con proveedores de servicios de confianza que nos ayudan a operar nuestro sitio web y prestar servicios, siempre y cuando se comprometan a mantener esta información confidencial.
+Requisitos Legales: Podemos divulgar tu información cuando sea necesario para cumplir con la ley, responder a procesos legales, o proteger los derechos, propiedad o seguridad de [Nombre del Sitio Web], nuestros usuarios u otros.
+5. Seguridad de la Información
+
+Adoptamos medidas de seguridad razonables para proteger la información que recopilamos contra el acceso, uso o divulgación no autorizados. Sin embargo, no podemos garantizar la seguridad absoluta de la información transmitida a través de Internet.
+
+6. Enlaces a Sitios de Terceros
+
+Nuestro sitio web puede contener enlaces a sitios web de terceros. No somos responsables de las prácticas de privacidad ni del contenido de esos sitios web. Te recomendamos leer las políticas de privacidad de cada sitio web que visites.
+
+7. Cambios en esta Política de Privacidad
+
+Nos reservamos el derecho de actualizar esta política de privacidad en cualquier momento. Te notificaremos sobre cualquier cambio publicando la nueva política de privacidad en esta página. Te recomendamos revisar esta política periódicamente para estar informado sobre cómo protegemos tu información.</p>
+            <br />
+        </div>
+      </section>
+    </div>
+  )
+}
